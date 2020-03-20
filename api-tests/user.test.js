6baf04203dc1f1be8f28c08ac6765df4e42fa548
Ahmed Rafiullah Khattak <khattak.ahmed@yahoo.com>
2020-03-20 18:04:24 +0500

    Added api testing using supertest

---
+++ api-tests/user.test.js
@@ -0,0 +1,15 @@
+/* eslint-env jest */
+const { app } = require('../app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+test('GETG /api/users/change-password | users change password should return 400 if no auth header present', async () => {
+  const response = await request.post('/api/users/change-password')
+  expect(response.status).toBe(400)
+  expect(response.body).toEqual({
+
+    status: 'failed',
+    reason: 'no authorization header present'
+
+  })
+})
