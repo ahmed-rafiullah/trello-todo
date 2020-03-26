557a03ac11cb60b6d1e924b0d39e0f2b63709285
Ahmed Rafiullah Khattak <khattak.ahmed@yahoo.com>
2020-03-26 18:38:34 +0500

    deleted and renamed file

--- seeds/testing/01_cleandb.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('todos').del()
-    .then(() => {
-      return knex('groupz').del()
-    }).then(() => {
-      return knex('users').del()
-    })
-}
