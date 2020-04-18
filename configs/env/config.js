194ff98d9fe8bb70ec6b0b71b076bcb303bb9c62
Ahmed Rafiullah <khattak.ahmed@yahoo.com>
2020-04-18 16:28:41 +0500

    fixed config to work in any environment, added test badge to read me file and changed workflow name

--- configs/env/config.js
+++ configs/env/config.js
@@ -27,47 +27,72 @@ if (result.error) {
   'JWT_SECRET'
 ].forEach(name => {
   if (!process.env[name]) {
+    console.log(name,process.env[name])
     throw new AppError(`Environment variable ${name} is missing`, 500, true)
   }
 })
 
-const envSchema = Joi.object().keys({
-  SERVER_PORT: Joi.number().integer().greater(0).required(),
+
+
+
+// create schemas to verify config object is correct
+
+const dbSchema = Joi.object().keys({
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().integer().greater(0).required(),
   DB_NAME: Joi.string().required(),
   DB_USER: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
-  JWT_SECRET: Joi.string().required()
+  DB_PASSWORD: Joi.string().required()
+}).required()
 
+
+const serverSchema = Joi.object().keys({
+  SERVER_PORT: Joi.number().integer().greater(0).required(),
+}).required()
+
+
+const securitySchema = Joi.object().keys({
+  JWT_SECRET: Joi.string().required(),
+  SALT_ROUNDS: Joi.number().integer().min(10)
+}).required()
+
+
+
+
+const envSchema = Joi.object().keys({
+  server: serverSchema,
+  db: dbSchema,
+  security: securitySchema
 }).unknown(false)
+
+// create config object
+const config = {
+  db: {
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD
+  },
+
+  server: {
+    SERVER_PORT: process.env.SERVER_PORT
+  },
+
+  security: {
+    JWT_SECRET: process.env.JWT_SECRET,
+    SALT_ROUNDS: 10
+  }
+}
 
 const {
   error,
   value: validEnv
-} = envSchema.validate(result.parsed)
+} = envSchema.validate(config)
 
 if (error) {
   throw new AppError(`Config validation error: ${error.message}`, 500, true)
 }
 
-const config = {
-  db: {
-    DB_HOST: validEnv.DB_HOST,
-    DB_PORT: validEnv.DB_PORT,
-    DB_NAME: validEnv.DB_NAME,
-    DB_USER: validEnv.DB_USER,
-    DB_PASSWORD: validEnv.DB_PASSWORD
-  },
-
-  server: {
-    SERVER_PORT: validEnv.SERVER_PORT
-  },
-
-  security: {
-    JWT_SECRET: validEnv.JWT_SECRET,
-    SALT_ROUNDS: 10
-  }
-}
 
 module.exports = config
